c99af609993d73be55163e4a4024e308592d2f40
Programmer9211 <adityachaudhary1029@gmail.com>
2020-12-06 15:54:18 +0530

    change firebase version and news feature

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,48 +1,74 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:not_whatsapp/models/userData.dart';
+import 'package:flutter/material.dart';
+import 'package:not_whatsapp/authen/authentication.dart';
+import 'package:not_whatsapp/home/home.dart';
 
-class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+FirebaseAuth auth = FirebaseAuth.instance;
 
-  Stream<User> get user {
-    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
-  }
+void signIn(String email, String password, BuildContext context,
+    GlobalKey<ScaffoldState> _scaffoldkey) async {
+  try {
+    final User user = (await auth.signInWithEmailAndPassword(
+            email: email, password: password))
+        .user;
 
-  User _userFromFirebaseUser(FirebaseUser user) {
-    return user != null ? User(uid: user.uid) : null;
-  }
+    if (user != null) {
+      if (!user.emailVerified) {
+        await user.sendEmailVerification(); // verification email aa jayega
+      }
 
-  Future signInWithemailPassword(String email, String password) async {
-    try {
-      AuthResult result = await _auth.signInWithEmailAndPassword(
-          email: email, password: password);
-      FirebaseUser user = result.user;
-      //await DatabaseService(uid: user.uid).updateUserData("0", "New User", 100);
-      return _userFromFirebaseUser(user);
-    } catch (e) {
-      print(e.toString());
-      return null;
+      print("${auth.currentUser.providerData}\n");
+
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (_) => Home()));
+    }
+  } catch (e) {
+    switch (e.message) {
+      case 'There is no user record corresponding to this identifier. The user may have been deleted.':
+        _scaffoldkey.currentState.showSnackBar(
+            SnackBar(content: Text("No User Found Please Create Account")));
+        break;
+      case 'The password is invalid or the user does not have a password.':
+        _scaffoldkey.currentState
+            .showSnackBar(SnackBar(content: Text("Invalid email or password")));
+        break;
+      case 'A network error (such as timeout, interrupted connection or unreachable host) has occurred.':
+        _scaffoldkey.currentState.showSnackBar(
+            SnackBar(content: Text("Check your internet connection")));
+        break;
+      // ...
+      default:
+        _scaffoldkey.currentState
+            .showSnackBar(SnackBar(content: Text("Unexpected error occured")));
     }
   }
+}
 
-  Future registerWithemailpassword(String email, String password) async {
-    try {
-      AuthResult result = await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
-      FirebaseUser user = result.user;
-      return _userFromFirebaseUser(user);
-    } catch (e) {
-      print(e.toString());
-      return null;
+void signUp(String email, String password, String userName,
+    BuildContext context) async {
+  final User user = (await auth.createUserWithEmailAndPassword(
+          email: email, password: password))
+      .user;
+
+  if (user != null) {
+    if (!user.emailVerified) {
+      await user.sendEmailVerification();
     }
+
+    await user.updateProfile(
+        displayName: userName); // user name update ho jayega
+
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (_) => Home()));
   }
+}
 
-  Future signOut() async {
-    try {
-      return await _auth.signOut();
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
+void signout(BuildContext context) async {
+  try {
+    await auth.signOut();
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (_) => Authentication()));
+  } catch (e) {
+    print(e);
   }
 }
